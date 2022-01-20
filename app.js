2fac3ecdbe5a46d50bb0c55c58fa38ce3f8d80b0
Dimitar <dimiturivanov92@gmail.com>
2022-01-20 12:44:23 +0200

    make it better

--- app.js
+++ app.js
@@ -21,12 +21,12 @@ const jwt = require('jsonwebtoken');
 const https = require('https');
 
 const app = express();
-const port = process.env.PORT
+const port = process.env.PORT || 5000
 
 app.use(express.json());
 
 // connect to mongodb and // listen for req
-mongoose.connect(process.env.MONGODB_URI || dbURI,  {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGODB_URI,  {useNewUrlParser: true, useUnifiedTopology: true})
     .then((result)=> app.listen(port))
     .catch((err)=> console.log(err))
 
