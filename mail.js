2012895cc91b02a77c3258a7c23070e329f88603
DimitarIvanov7 <94909532+DimitarIvanov7@users.noreply.github.com>
2022-01-22 23:16:12 +0200

    Update mail.js

--- mail.js
+++ mail.js
@@ -3,8 +3,8 @@ const mailGun = require('nodemailer-mailgun-transport');
 
 const auth = {
     auth: {
-        api_key: '0032fe0cc2762c88b58397edb12631f4-ef80054a-40225d40',
-        domain: 'sandbox0b805705e54940ac818e2bd7be6b8de8.mailgun.org'
+        api_key: 'API_KEY',
+        domain: 'MY_DOMAIN'
     }
 };
 
