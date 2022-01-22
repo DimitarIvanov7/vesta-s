82527e4d19dac08651d7d6458fa4fdd0f4aeebd6
DimitarIvanov7 <94909532+DimitarIvanov7@users.noreply.github.com>
2022-01-22 23:18:24 +0200

    Update users-mod.js

--- models/users-mod.js
+++ models/users-mod.js
@@ -2,7 +2,7 @@ const { builtinModules } = require('module');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const dbURI = 'mongodb://dimitar:1234@cluster0-shard-00-00.p4jdz.mongodb.net:27017,cluster0-shard-00-01.p4jdz.mongodb.net:27017,cluster0-shard-00-02.p4jdz.mongodb.net:27017/Vesta-s?ssl=true&replicaSet=atlas-5kz6i3-shard-0&authSource=admin&retryWrites=true&w=majority'
+const dbURI = 'API_KEY'
 
 
 const userSchema = new Schema({
